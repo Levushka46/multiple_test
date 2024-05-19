2c164b56bfc71b7057951b4fc615ff700e4875ee
Lev Sergeev <levushka46@mail.ru>
2024-05-19 15:33:16 +0300

    Initial commit with project setup and GitHub Actions workflow

---
+++ tests/test_module.py
@@ -0,0 +1,6 @@
+from src.module import multiplication
+
+def test_multiplication():
+    assert multiplication(1, 2) == 2
+    assert multiplication(5, 5) == 25
+    assert multiplication(7, 7) == 49
